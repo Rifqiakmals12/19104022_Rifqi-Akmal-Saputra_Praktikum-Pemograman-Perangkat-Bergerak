b572832b134cd0a54a79fe70429fa3ad81bf9987
Rifqiakmals12 <rifqias1212@gmail.com>
2022-01-22 06:25:08 +0700

    Modul 3 (Hello World dan Activity Lifecycle

---
+++ Modul 3/Hello IT Telkom Purwokerto/app/src/main/java/com/rifqiakmals_19104022/helloittelkompurwokerto/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.rifqiakmals_19104022.helloittelkompurwokerto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
